f48438d99b6a939fe8dfc13d2713c58495c2cca8
Alok Mrityunjay <alok.mrityunjay1992@gmail.com>
2019-10-28 00:01:06 +0530

    Internationalization in Spring boot

--- src/main/java/com/practice/microservices/helloworld/HelloWorldController.java
+++ src/main/java/com/practice/microservices/helloworld/HelloWorldController.java
@@ -1,5 +1,8 @@
 package com.practice.microservices.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
+	@Autowired
+	MessageSource messageSource;
+	
 	@GetMapping(path = "/open-sesame")
 	public String openSesame() {
 		return "Open sesame";
@@ -20,5 +26,10 @@ public class HelloWorldController {
 	@GetMapping(path = "/hello-world/path/{name}")
 	public HelloWorldBean helloWorldPath(@PathVariable String name) {
 		return new HelloWorldBean(String.format("Hello World , %s", name));
+	}
+	
+	@GetMapping(path = "/hello-world-internationalized")
+	public String helloWorldInternationalized() {
+		return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
 	}
 }
