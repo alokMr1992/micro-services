3edfd9d8e5613294a7393e8afad603a53ab211cc
Alok Mrityunjay <alok.mrityunjay1992@gmail.com>
2019-11-03 01:24:48 +0530

    Add more info to Swagger document

--- src/main/java/com/practice/microservices/SwaggerConfig.java
+++ src/main/java/com/practice/microservices/SwaggerConfig.java
@@ -1,8 +1,14 @@
 package com.practice.microservices;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -11,8 +17,16 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 
+	public static final Contact DEFAULT_CONTACT = new Contact("Alok Mrityunjay", "https://github.com/alokMr1992",
+			"alok.mrityunjay1992@gmail.com");
+	public static final ApiInfo MY_API_INFO = new ApiInfo("Microservice API", "Microservice with Spring cloud", "1.0",
+			"urn:tos", DEFAULT_CONTACT, "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0");
+	private static final Set<String> PRODUCES_AND_CONSUMES = new HashSet<>(
+			Arrays.asList("application/json", "application/xml"));
+
 	@Bean
 	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2);
+		return new Docket(DocumentationType.SWAGGER_2).apiInfo(MY_API_INFO).produces(PRODUCES_AND_CONSUMES)
+				.consumes(PRODUCES_AND_CONSUMES);
 	}
 }
