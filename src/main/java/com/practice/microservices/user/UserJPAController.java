142b47dd03e5c0eec9e2ff99cfbb0366e3d7458a
Alok Mrityunjay <alok.mrityunjay1992@gmail.com>
2021-05-02 22:52:56 +0530

    JPA Intro

---
+++ src/main/java/com/practice/microservices/user/UserJPAController.java
@@ -0,0 +1,67 @@
+package com.practice.microservices.user;
+
+import java.net.URI;
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.*;
+
+@RestController
+public class UserJPAController {
+
+	@Autowired
+	private UserDaoService userDaoService;
+	
+	@Autowired
+	private UserRepository userRepository;
+
+	@GetMapping("/jpa/users")
+	public List<User> retrieveAllUser() {
+		return userRepository.findAll();
+	}
+
+	@GetMapping("/jpa/users/{id}")
+	public Resource<User> retrieveUser(@PathVariable int id) {
+		User user = userDaoService.findOne(id);
+		if (null == user) {
+			throw new UserNotFoundException("id = " + id);
+		}
+
+		Resource<User> resource = new Resource<User>(user);
+		ControllerLinkBuilder linkBuilder = linkTo(methodOn(this.getClass()).retrieveAllUser());
+		resource.add(linkBuilder.withRel("all-users"));
+
+		return resource;
+	}
+
+	@PostMapping("/jpa/users")
+	public ResponseEntity<Object> createUser(@Valid @RequestBody User user) {
+		User savedUser = userDaoService.save(user);
+
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId())
+				.toUri();
+		return ResponseEntity.created(location).build();
+	}
+
+	@DeleteMapping("/jpa/users/{id}")
+	public void deleteUser(@PathVariable int id) {
+		User user = userDaoService.deleteUserById(id);
+
+		if (null == user) {
+			throw new UserNotFoundException("id = " + id);
+		}
+	}
+}
